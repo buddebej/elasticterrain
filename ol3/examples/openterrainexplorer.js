af81293f468b5781af6576d3609cee31d14cb338
buddebej <buddebej@mailbox.org>
2015-02-05 15:14:41 -0800

    rename example to openterrainexplorer and change folder structure

--- ol3/examples/openterrainexplorer.js
+++ ol3/examples/openterrainexplorer.js
@@ -1,15 +1,30 @@
+goog.require('ol.Map');
+goog.require('ol.View');
+goog.require('ol.control');
+goog.require('ol.control.ScaleLine');
+goog.require('ol.source.XYZ');
+goog.require('ol.layer.Tile');
+goog.require('ol.layer.TileDem');
+goog.require('ol.proj');
+goog.require('ol.source.OSM');
+goog.require('ol.source.Stamen');
+goog.require('ol.source.BingMaps');
+goog.require('ol.source.TileDebug');
+
+goog.require('ol.dom.Input');
+
 $(document).ready(function() {
     'use strict';
     if (webgl_detect()) {
 
-        var dem, osm, stamen, bing, vector, ol3DemMap, ol3demUi;
+        var dem, osm, stamen, bing, debug, vector, ol3DemMap, ol3demUi;
 
         dem = new ol.layer.TileDem({
             source: new ol.source.XYZ({
                 attributions: [new ol.Attribution({
                     html: '<a href="http://www.eea.europa.eu/data-and-maps/data/eu-dem" target="_blank">Produced using Copernicus data and information funded by the European Union - EU-DEM layers</a>'
                 })],
-                url: 'tiles/{z}/{x}/{y}.png'
+                url: '../demo/tiles/{z}/{x}/{y}.png'
             })
         });
 
@@ -33,11 +48,27 @@ $(document).ready(function() {
         });
         bing.t = "Bing Aerial";
 
+        debug = [
+            new ol.layer.Tile({
+              source: new ol.source.OSM()
+            }),
+            new ol.layer.Tile({
+              source: new ol.source.TileDebug({
+                projection: 'EPSG:3857',
+                tileGrid: new ol.tilegrid.XYZ({
+                  maxZoom: 22
+                })
+              })
+            })
+          ];
+
         ol3DemMap = new ol.Map({
             controls: ol.control.defaults().extend([new ol.control.ScaleLine()]),
             target: 'map',
             renderer: 'webgl',
             layers: [osm, stamen, bing, dem], // base dem has always to be the last added layer
+            // renderer: exampleNS.getRendererFromQueryString(),
+            // layers: debug,
             view: new ol.View({ center: ol.proj.transform([7.754974, 46.375803], 'EPSG:4326', 'EPSG:3857'), // alps
                                 zoom: 10,
                                 maxZoom: 12,
@@ -52,7 +83,7 @@ $(document).ready(function() {
             var elevation = ol3DemMap.getRenderer().getLayerRenderer(dem).getElevation(coord,ol3DemMap.getView().getZoom());
             console.log(transformed_coordinate,elevation+' meters');
         });
-        
+
     } else {
         $('body').append('<div class="webglMissing"><p><span class="title">WebGL Not Supported!</span><br> WebGL is required for this application, and your Web browser does not support WebGL. Google Chrome or Firefox are recommended browsers with WebGL support. Click <a href="http://www.browserleaks.com/webgl" target="_blank">here</a> to check the WebGL specifications of your browser.</p></div>');
     }
