4769580816c343c8298a0ca4a6df7889d02e015a
buddebej <buddebej@mailbox.org>
2015-04-29 20:13:53 +0200

    add title to model

--- backend/models/config.js
+++ backend/models/config.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var configSchema = new Schema({
+    title: String,
     ambientLight: Number,
     colorScale: [Number],
     colorRamp: Number,
