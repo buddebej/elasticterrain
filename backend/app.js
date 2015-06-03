5eebb860b91024064719c0136a6e5464d238cc4f
buddebej <buddebej@mailbox.org>
2015-06-03 11:45:22 +0200

    modify wildcard

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ var connectionString = 'mongodb://localhost:27017/' + dbName;
 mongoose.connect(connectionString);
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://*.elasticterrain.xyz');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
