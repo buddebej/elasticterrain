7247eb12aeaca236e082912c9da2e119981b7684
buddebej <buddebej@mailbox.org>
2015-08-25 18:22:39 +0200

    add functionality to sort presets

--- www/src/js/configmanager.js
+++ www/src/js/configmanager.js
@@ -59,6 +59,11 @@ var ConfigManager = function(viewer, controlBar, initShowCase) {
             type: 'GET',
             url: this.url,
             success: function(data) {
+                data.sort(function(a, b) {
+                    var aOrder = a.hasOwnProperty('order') ? a.order : 99;
+                    var bOrder = b.hasOwnProperty('order') ? b.order : 99;
+                    return ((aOrder < bOrder) ? -1 : ((aOrder > bOrder) ? 1 : 0));
+                });
                 viewer.setConfigStore(data);
                 if (typeof(controlBar) !== 'undefined') {
                     controlBar.updateConfigStore();
