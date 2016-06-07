f0112f04c14648e6cc8fbb40e08d851d6cb1094b
buddebej <buddebej@mailbox.org>
2016-06-07 23:27:32 +0200

    remove backend

--- backend/bin/www.js
+++
@@ -1,7 +0,0 @@
-var app = require('../app'); 
- 
-app.set('port', process.env.PORT || 8000);
- 
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address() + ' ' +server.address().port);
-});
